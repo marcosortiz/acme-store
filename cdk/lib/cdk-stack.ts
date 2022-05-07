572de174926d05e60a03d0c0bb2c865ed934da46
marcos ortiz <my@email.com>
2022-05-07 17:38:42 -0500

    removing comments

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,9 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
-// import * as ecr from "aws-cdk-lib/aws-ecr-assets";
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
-// import * as path from 'path';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -18,19 +16,11 @@ export class CdkStack extends Stack {
       vpc: vpc
     });
 
-    // const serviceA = new ecr.DockerImageAsset(this, 'ServiceA', {
-    //   directory: path.join(__dirname, '..', '..','services','serviceA'),
-    //   networkMode: ecr.NetworkMode.HOST,
-    // });
-
     // Create a load-balanced Fargate service and make it public
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "ServiceA", {
       cluster: cluster, // Required
       cpu: 512, // Default is 256
       desiredCount: 6, // Default is 1
-      // taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
-      
-      // taskImageOptions: { image: ecs.ContainerImage.fromDockerImageAsset(serviceA) },
       taskImageOptions: { 
         image: ecs.ContainerImage.fromAsset("/Users/ormarcos/dev/aws-containers-labs/services/serviceA/"),
         containerPort: 3000 
