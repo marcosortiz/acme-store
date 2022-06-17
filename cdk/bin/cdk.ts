f9a9dc8599e5487bbbb3fada6fa7f1e08bc3fce9
marcos ortiz <my@email.com>
2022-06-17 08:13:18 -0500

    Renaming cdk stack.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,6 +5,7 @@ import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack', {
+  stackName: 'AcmeStoreCdkStack'
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
