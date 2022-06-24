b63464d68a58d9948688ea5439a31b7892615db2
Marcos Ortiz <johndoe@example.com>
2022-06-24 23:24:44 +0000

    Using pg-migrate.

---
+++ src/orders/bin/db/drop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mydir="${0%/*}"
+CONFIG_FILE=$mydir/../../config/default.json
+
+export DB_CONFIG=$(cat $CONFIG_FILE | jq '.db')
+export PGHOST=$(echo $DB_CONFIG | jq -r '.host')
+export PGPORT=$(echo $DB_CONFIG | jq -r '.port')
+export PGUSER=$(echo $DB_CONFIG | jq -r '.username')
+export PGPASSWORD=$(echo $DB_CONFIG | jq -r '.password')
+export PGDATABASE=postgres
+export DB_NAME=$(echo $DB_CONFIG | jq -r '.database')
+
+echo "Dropping database \"$DB_NAME\" ..."
+psql -f $mydir/drop-db.sql -v dbname=$DB_NAME
--- src/orders/bin/db/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mydir="${0%/*}"
-CONFIG_PATH=$mydir/../../../../config
-SCRIPT_PATH=$mydir/../..
-
-NODE_CONFIG_DIR=$CONFIG_PATH node $SCRIPT_PATH/testConnection.js
