bfb4d2489a0882f69a076594863f3a24c82559ad
Marcos Ortiz <johndoe@example.com>
2022-06-25 00:31:37 +0000

    Adding orders table.

--- src/orders/migrations/1656087721501_users.js
+++ src/orders/migrations/1656087721501_users.js
@@ -3,17 +3,35 @@
 exports.shorthands = undefined;
 
 exports.up = pgm => {
-    pgm.createTable('users', {
-        id: 'id',
-        username: { type: 'varchar(1000)', notNull: true },
-        createdAt: {
-          type: 'timestamp',
-          notNull: true,
-          default: pgm.func('current_timestamp'),
-        },
-      });
+  pgm.createTable('users', {
+    id: 'id',
+    username: { type: 'varchar(1000)', notNull: true },
+    createdAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+
+  pgm.createTable('orders', {
+    id: 'id',
+    userId: {
+      type: 'integer',
+      notNull: true,
+      references: '"users"',
+      onDelete: 'cascade',
+    },
+    details: { type: 'text', notNull: true },
+    createdAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  })
+  pgm.createIndex('orders', 'userId')
 };
 
 exports.down = pgm => {
-    pgm.dropTable('users', {});
+  pgm.dropTable('orders', {});
+  pgm.dropTable('users', {});
 };
